8a6a198f893805c912835dff9a5cc6a34fe7f150
ElectNewt <ivanxo69@gmail.com>
2023-08-16 08:12:12 +0100

    unit testing with ef core and dbcontext

---
+++ 02-CodeFirst/src/CursoEFCore.Codefirst/CursoEFCore.Codefirst.API/Services/InsertUserDbContext.cs
@@ -0,0 +1,52 @@
+﻿using CursoEFCore.Codefirst.API.Data;
+using CursoEFCore.Codefirst.API.Data.Entities;
+
+namespace CursoEFCore.Codefirst.API.Services;
+
+//This class is used only to show how the tests with DBContext works
+public class InsertUserDbContext
+{
+    private readonly CursoEfContext _context;
+
+    public InsertUserDbContext(CursoEfContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Execute(int id)
+    {
+        User user = new User()
+        {
+            Email = $"{Guid.NewGuid()}@mail.com",
+            UserName = $"id{id}"
+        };
+
+        List<Wokringexperience> workingExperiences = new List<Wokringexperience>()
+        {
+            new Wokringexperience()
+            {
+                User = user,
+                Name = $"experience1 user {id}",
+                Details = "details1",
+                Environment = "environment"
+            },
+            new Wokringexperience()
+            {
+                User = user,
+                Name = $"experience user {id}",
+                Details = "details2",
+                Environment = "environment"
+            }
+        };
+
+        if (_context.Users.Any(a => a.UserName == user.UserName))
+            return false;
+        
+
+        _ = await _context.Users.AddAsync(user);
+        await _context.Wokringexperiences.AddRangeAsync(workingExperiences);
+        _ = await _context.SaveChangesAsync();
+
+        return true;
+    }
+}
---
+++ 02-CodeFirst/src/CursoEFCore.Codefirst/CursoEFCore.Codefirst.Test/Services/TestInsertUserDbContext.cs
@@ -0,0 +1,97 @@
+﻿using CursoEFCore.Codefirst.API.Data;
+using CursoEFCore.Codefirst.API.Data.Entities;
+using CursoEFCore.Codefirst.API.Services;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+
+namespace CursoEFCore.Codefirst.Test.Services;
+
+public class TestInsertUserDbContext
+{
+    [Fact]
+    public async Task WhenUsernameDoesNotExist_ThenUserInserted()
+    {
+        CursoEfContext context = Substitute.For<CursoEfContext>(new DbContextOptions<CursoEfContext>());
+
+
+        DbSet<User> userDbSet = FakeDbSet(new List<User>());
+        context.Users.Returns(userDbSet);
+
+        DbSet<Wokringexperience> dbSetWorkingExperiences = FakeDbSet(new List<Wokringexperience>());
+        context.Wokringexperiences.Returns(dbSetWorkingExperiences);
+
+        InsertUserDbContext subject = new InsertUserDbContext(context);
+
+        var result = await subject.Execute(10);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task WhenUsernameDoesExist_ThenNotInserted()
+    {
+        int id = 1;
+        CursoEfContext context = Substitute.For<CursoEfContext>(new DbContextOptions<CursoEfContext>());
+
+
+        DbSet<User> userDbSet = FakeDbSet(new List<User>() { new User() { UserName = $"id{id}" } });
+        context.Users.Returns(userDbSet);
+
+        InsertUserDbContext subject = new InsertUserDbContext(context);
+
+        var result = await subject.Execute(id);
+
+        Assert.False(result);
+    }
+
+    private static DbSet<T> FakeDbSet<T>(List<T> data) where T : class
+    {
+        var _data = data.AsQueryable();
+        var fakeDbSet = Substitute.For<DbSet<T>, IQueryable<T>>();
+        ((IQueryable<T>)fakeDbSet).Provider.Returns(_data.Provider);
+        ((IQueryable<T>)fakeDbSet).Expression.Returns(_data.Expression);
+        ((IQueryable<T>)fakeDbSet).ElementType.Returns(_data.ElementType);
+        ((IQueryable<T>)fakeDbSet).GetEnumerator().Returns(_data.GetEnumerator());
+
+        fakeDbSet.AsQueryable().Returns(fakeDbSet);
+
+        return fakeDbSet;
+    }
+
+    [Fact]
+    public async Task WhenUsernameDoesNotExist_ThenUserInsertedInDatabase()
+    {
+        CursoEfContext context = GetInMemoryDbContext();
+        InsertUserDbContext subject = new InsertUserDbContext(context);
+
+        var result = await subject.Execute(10);
+
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public async Task WhenUsernameDoesExist_ThenNotInsertedInDatabase() //inMemory
+    {
+        int id = 1;
+        CursoEfContext context = GetInMemoryDbContext();
+        await context.Users.AddAsync(new User() { UserName = $"id{id}", Email = "mail@mail.com"});
+        await context.SaveChangesAsync();
+
+        InsertUserDbContext subject = new InsertUserDbContext(context);
+        var result = await subject.Execute(id);
+
+        Assert.False(result);
+    }
+
+    private CursoEfContext GetInMemoryDbContext()
+    {
+        DbContextOptions<CursoEfContext> databaseOptions = new DbContextOptionsBuilder<CursoEfContext>()
+            .UseInMemoryDatabase("CursoDatabase")
+            .Options;
+        
+        return new CursoEfContext(databaseOptions);
+    }
+    
+    
+    
+}
