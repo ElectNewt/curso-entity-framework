4614812330f6847ebcebc1d33165ce1e2eb3a62d
Netmentor <ivanxo69@gmail.com>
2025-08-04 10:58:18 +0100

    wip for blogpost

--- 02-CodeFirst/src/CursoEFCore.Codefirst/CursoEFCore.Codefirst.API/Migrations/20250804093531_IncludeOwnedAddress.Designer.cs
+++ 02-CodeFirst/src/CursoEFCore.Codefirst/CursoEFCore.Codefirst.API/Migrations/20250804093531_IncludeOwnedAddress.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CursoEFCore.Codefirst.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CursoEFCore.Codefirst.API.Migrations
 {
     [DbContext(typeof(CursoEfContext))]
-    partial class CursoEfContextModelSnapshot : ModelSnapshot
+    [Migration("20250804093531_IncludeOwnedAddress")]
+    partial class IncludeOwnedAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
